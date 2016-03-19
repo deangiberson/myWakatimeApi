154c7f338f8d4e92eab558ab746b603b320da0b9
Dean Giberson <dean@deangiberson.com>
2016-03-18 21:54:34 -0700

    Render to user json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,16 @@ class UsersController < ApplicationController
   before_filter :restrict_access
 
   def show
-    @user = User.find_by(username: params[:user])
     render json: @user
   end
 
   def current
+    render json: @user
   end
 
   def restrict_access
     authenticate_or_request_with_http_token do |token, options|
-      User.find_by(user_id: token)
+      @user = User.find_by(user_id: token)
     end
   end
 end
