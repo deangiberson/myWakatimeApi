c5e2fd2d4b641596f4104aeb2f6363938cc38f08
Dean Giberson <dean@deangiberson.com>
2016-03-25 22:56:29 -0700

    Forcing fixed api keys to ease testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,14 +38,19 @@ users = User.create([{username: 'deangiberson',
 
 p users
 
-apikey = ApiKey.create([{user: users[0],
-                         created_at: "2016-03-17 06:22:42",
-                         updated_at: "2016-03-17 06:22:42"},
-                        {user: users[1],
-                         created_at: "2016-03-17 06:22:42",
-                         updated_at: "2016-03-17 06:22:42"},
-                        {user: users[2],
-                         created_at: "2016-03-17 06:22:42",
-                         updated_at: "2016-03-17 06:22:42"}])
+apikeys = ApiKey.create([{user: users[0],
+                          created_at: "2016-03-17 06:22:42",
+                          updated_at: "2016-03-17 06:22:42"},
+                         {user: users[1],
+                          created_at: "2016-03-17 06:22:42",
+                          updated_at: "2016-03-17 06:22:42"},
+                         {user: users[2],
+                          created_at: "2016-03-17 06:22:42",
+                          updated_at: "2016-03-17 06:22:42"}])
 
-p apikey
+# Forcing these accoutns to have fixed api keys to ease testing
+apikeys[0].access_token = "f912b2e9f764ac031f137f6ecf84ebca"
+apikeys[1].access_token = "88144fc51182cfa77a6ddae039dab97a"
+apikeys[2].access_token = "a3ae63b5b8dfffe8a18680b467d5d1da"
+
+p apikeys
