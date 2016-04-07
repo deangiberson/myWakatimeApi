a17bd1a091fe08708ec00441348245794e7b7e0e
Dean Giberson <dean@deangiberson.com>
2016-04-06 22:37:37 -0700

    Stat model accessors

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,3 +1,8 @@
 class Stat < ActiveRecord::Base
   belongs_to :user
+  has_one :best_day, class_name: "StatBestDay", inverse_of: :stat
+  has_many :editors, class_name: "StatEditor", inverse_of: :stat
+  has_many :languages, class_name: "StatLanguages", inverse_of: :stat
+  has_many :operating_systems, class_name: "StatOperatingSystem", inverse_of: :stat
+  has_many :projects, class_name: "StatProject", inverse_of: :stat
 end
